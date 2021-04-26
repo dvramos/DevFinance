d1be328bf06caa4afecdc42990a2e2c4261b5c6c
dvramos <dvramos@hotmail.com.br>
2021-04-26 14:13:39 -0300

    Aula 03
    Adicionado Script Transaction

--- script.js
+++ script.js
@@ -60,11 +60,13 @@ const DOM = {
 
         },
     innerHTMLTransaction(transaction){
+        const CSSclass = transaction.amount > 0 ? "income" : "expense" // se amount maior que 0 vá para income senão vá para expense
 
+        const amount = Utils.formatCurrency(transaction.amount)
 
         const html = `
                         <td class="description">${transaction.description}</td>
-                        <td class="expense">${transaction.amount}</td>
+                        <td class="${CSSclass}">${amount}</td>
                         <td class="date">${transaction.date}</td>
                         <td><img src="assets/minus.svg"   alt="Remover Transação"></td>
                     `
@@ -72,4 +74,24 @@ const DOM = {
     }
 }
 
-DOM.addTransaction(transactions[0])
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+    
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-Br", {
+            style: "currency",
+            currency: "BRL"
+        })
+        
+        return signal + value
+    
+    }
+}
+
+transactions.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+})
